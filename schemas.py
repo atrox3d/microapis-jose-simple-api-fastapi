08f80c7811026ea90095deed5ebc6c963a50da9e
atrox3d <atrox3d@gmail.com>
2024-09-13 10:50:13 +0200

    schemas.py

---
+++ schemas.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from enum import StrEnum, auto
+from uuid import UUID
+from pydantic import BaseModel
+
+class Error(BaseModel):
+    detail: str | None = None
+
+
+class Priority(StrEnum):
+    LOW = auto()
+    MEDIUM = auto()
+    HIGH = auto()
+
+
+class Status(StrEnum):
+    PENDING = auto()
+    PROGRESS = auto()
+    COMPLETED = auto()
+
+
+class CreateTaskSchema(BaseModel):
+    priority: Priority | None  = Priority.LOW
+    status: Status | None  = Status.PENDING
+
+
+class GetTaskSchema(CreateTaskSchema):
+    id: UUID
+    created: datetime
+    task: str
+
+
+class ListTasksSchema(BaseModel):
+    tasks: list[GetTaskSchema]
