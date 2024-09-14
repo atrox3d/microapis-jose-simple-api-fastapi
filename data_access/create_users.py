358f107c8b5b63f5763674bcc03a2a64e15984b0
atrox3d <atrox3d@gmail.com>
2024-09-14 08:29:28 +0200

    create example users

--- data_access/create_users.py
+++ data_access/create_users.py
@@ -10,7 +10,30 @@ users = [
 # create contex manager to get session
 session_maker = sessionmaker(bind=create_engine('sqlite:///models.db'))
 
-with session_maker() as session:
-    # session.query(User).delete()
-    session.add_all(users)
-    session.commit()
+def delete_db_users():
+    with session_maker() as session:
+        session.query(User).delete()
+        session.commit()
+
+def create_db_users(users:list[User]):
+    with session_maker() as session:
+        session.add_all(users)
+        session.commit()
+
+def print_db_users():
+    with session_maker() as session:
+        dbusers = session.query(User).all()
+        for user in dbusers:
+            print(user.dict())
+
+def create_users(users:list[User], *, delete:bool, print:bool=True):
+    if delete:
+        delete_db_users()
+    
+    create_db_users(users)
+
+    if print:
+        print_db_users()
+
+if __name__ == "__main__":
+    create_users(users, delete=True)
